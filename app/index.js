e2f843561ecc5d0b71a641237805a3bd98e19825
Tabhi109 <tabhinav109@gmail.com>
2023-06-02 00:14:49 +0530

    created HelloWorld mobile app

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+const Home = () => {
+    return (
+        <View>
+        <Text>Hello World</Text>
+        </View>
+    );
+    }
+    export default Home;
