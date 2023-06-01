4d842e7f71c9171d54ce9d6f3f68e55fe3940cb2
Tabhi109 <tabhinav109@gmail.com>
2023-06-02 00:48:36 +0530

    updated app styles !

--- app/index.js
+++ app/index.js
@@ -1,9 +1,18 @@
-import { View, Text } from 'react-native';
+import { Text,View, ScrollView , SafeAreaView } from 'react-native';
+import { Stack,useRouter } from 'expo-router';
+import { useState } from 'react';
+import {COLORS, icons , images , SIZES , FONTS} from '../constants';
+import { Nearbyjobs , Popularjobs , ScreenHeaderBtn , Welcome  } from '../components';
+
 const Home = () => {
+    const router = useRouter();
+
     return (
-        <View>
-        <Text>Hello World</Text>
-        </View>
+        <SafeAreaView style={{ flex:1,backgroundColor:COLORS.lightWhite}}>
+        <Stack.Screen options={{
+            headerStyle: { backgroundColor: COLORS.lightWhite },
+        }}/><Text>Hello</Text>
+        </SafeAreaView>
     );
     }
     export default Home;
